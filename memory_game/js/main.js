e06ef9e874a76c88398f1c78a62ecd628d6bf1ff
Heggyhere <heggyy@gmail.com>
2019-01-14 17:51:12 -0800

    created obj to display more info about card

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,5 +1,26 @@
 // Create an array cards.
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+  {
+    rank: "queen",
+    suit: "hearts",
+    cardImage: "images/queen-of-hearts.png"
+  },
+  {
+    rank: "queen",
+    suit: "diamonds",
+    cardImage: "images/queen-of-diamonds.png"
+  }, 
+  {
+    rank: "king",
+    suit: "hearts",
+    cardImage: "images/king-of-hearts.png"
+  }, 
+  {
+    rank: "king",
+    suit: "diamonds",
+    cardImage: "images/king-of-diamonds.png"
+  }
+];
 // current player's hand
 var cardsInPlay = [];
 // function to store the steps to check for a match
@@ -15,9 +36,13 @@ var checkForMatch = function() {
 - accepts 1 parameter: cardId (indexNumber)
 */
 var flipCard = function(cardId) {
-  console.log("User flipped " + cards[cardId]);
-  cardsInPlay.push(cards[cardId]);
+  console.log("User flipped " + cards[cardId].rank);
+  // push name of card ie: king 
+  cardsInPlay.push(cards[cardId].rank);
   // when user flip both two cards
+  console.log(cards[cardId].cardImage);
+  console.log(cards[cardId].suit);
+
   if (cardsInPlay.length === 2) {
     checkForMatch();
   }
